6e7ab9dc91efc3158d477605d9b170c69f903895
AndresCampos21 <andres.campos6485@alumnos.udg.mx>
2025-08-22 11:12:29 -0600

    Update din_ipend.m

--- din_ipend.m
+++ din_ipend.m
@@ -10,8 +10,8 @@ G = 9.81;
 Bp = 0.0024;
 
 %x(1) = x
-%x(2) = alpha
-%x(3) = xdot
+%x(2) = xdot
+%x(3) = alpha
 %x(4) = alphadot
 
 Den = (Mc+Mp)*Ip+Mc*Mp*Lp^2+Mp^2*Lp^2*sin(x(3))^2;
@@ -20,3 +20,4 @@ dx(1) = x(2);
 dx(2) = ((Ip+Mp*Lp^2)*Fc+Mp^2*Lp^2*G*cos(x(3))*sin(x(3))-(Ip+Mp*Lp^2)*Beq*x(2)-(Ip*Mp*Lp-Mp^2*Lp^3)*x(4)^2*sin(x(3))-Mp*Lp*x(4)*cos(x(3))*Bp)/Den;
 dx(3) = x(4);
 dx(4) = ((Mc+Mp)*Mp*G*Lp*sin(x(3))-(Mc+Mp)*Bp*x(4)+Fc*Mp*Lp*cos(x(3))-Mp^2*Lp^2*x(4)^2*sin(x(3))*cos(x(3))-Beq*Mp*Lp*x(2)*cos(x(3)))/Den;
+
